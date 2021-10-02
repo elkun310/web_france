8bb2a57d91d6b6a51b27ca490758e473deee695f
elkun310 <nguyenha9772@ymail.com>
2021-10-02 14:25:30 +0700

    first init

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer id="footer">
+    <ul class="icons">
+        <li>
+            <a target="_blank" href="https://twitter.com/lesbleusvietnam" class="icon fa-twitter">
+                <span class="label">twitter</span>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="https://www.facebook.com/FranceTeamVN" class="icon fa-facebook-official">
+                <span class="label">facebook-official</span>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="https://www.instagram.com/lesbleus.vn" class="icon fa-instagram">
+                <span class="label">instagram</span>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="https://www.youtube.com/user/LesBleusVN" class="icon fa-youtube-square">
+                <span class="label">youtube-square</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="copyright">
+        <li>&copy;2014 - 2021 Les Bleus Vietnam</li>
+    </ul>
+</footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header id="header" class="alt">
+        <h1><a href="http://doituyenphap.com/">Les Bleus Vietnam</a></h1>
+    </header>
+</template>
